ae8f44c06cb539d7ecd2351f95a30536477cc978
Mickey Gousset <migousse@microsoft.com>
2018-11-20 11:48:36 -0600

    still testing

--- scripts/ci/precheck_pep8.sh
+++ scripts/ci/precheck_pep8.sh
@@ -8,8 +8,8 @@ python -m pip install -qqq flake8
 
 echo "Run flake8."
 echo "env variables"
-echo '$Build.Reason'
-echo '$Build.Repository.Name'
+echo '$(Build.Reason)'
+echo '${Build.Repository.Name}'
 
 flake8 --statistics --config=./.flake8
 #flake8 --statistics --config=./.flake8 
