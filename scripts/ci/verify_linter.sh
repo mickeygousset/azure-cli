4fbf70018bb77b16f9e9c23968dae7307fdfaf38
Mickey Gousset <migousse@microsoft.com>
2018-11-20 14:34:04 -0600

    Working on linter verify stage

--- scripts/ci/verify_linter.sh
+++ scripts/ci/verify_linter.sh
@@ -2,9 +2,16 @@
 
 set -e
 
+echo 'mickey: start of verify linter'
+
 . $(cd $(dirname $0); pwd)/artifacts.sh
 
+echo 'mickey: share folder:'
+echo $share_folder
+
 ls -la $share_folder/build
+
+echo 'Mickey: All Modules'
 
 ALL_MODULES=`find $share_folder/build/ -name "*.whl"`
 
