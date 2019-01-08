c29c5f051b3fa36134701f6b2eb62419bdee57e6
Mickey Gousset <migousse@microsoft.com>
2019-01-08 14:40:37 -0600

    testing

--- scripts/ci/verify_linter.sh
+++ scripts/ci/verify_linter.sh
@@ -5,21 +5,21 @@ set -e
 echo 'mickey: start of verify linter'
 echo 'mickey: rocks'
 
-. $(cd $(dirname $0); pwd)/artifacts.sh
+#. $(cd $(dirname $0); pwd)/artifacts.sh
 
 echo 'mickey: rocks2'
 
-echo 'mickey: share folder:'
-echo $share_folder
+#echo 'mickey: share folder:'
+#echo $share_folder
 
-ls -la $share_folder/build
+#ls -la $share_folder/build
 
-echo 'Mickey: All Modules'
+#echo 'Mickey: All Modules'
 
-ALL_MODULES=`find $share_folder/build/ -name "*.whl"`
+#ALL_MODULES=`find $share_folder/build/ -name "*.whl"`
 
-pip install -e ./tools
-[ -d privates ] && pip install -qqq privates/*.whl
-pip install $ALL_MODULES
+#pip install -e ./tools
+#[ -d privates ] && pip install -qqq privates/*.whl
+#pip install $ALL_MODULES
 
-azdev cli-lint --ci
+#azdev cli-lint --ci
