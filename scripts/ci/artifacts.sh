cccfee51ed381245674f8c0d95fda3290524844a
Mickey Gousset <migousse@microsoft.com>
2019-01-08 15:42:07 -0600

    test

--- scripts/ci/artifacts.sh
+++ scripts/ci/artifacts.sh
@@ -1,4 +1,6 @@
 # Build wheel and set the $share_folder to the artifacts folder
 
+echo "before build.sh"
 . $(cd $(dirname $0); pwd)/build.sh
+echo "after build.sh"
 share_folder=$(cd $(dirname $0); cd ../../artifacts; pwd)
